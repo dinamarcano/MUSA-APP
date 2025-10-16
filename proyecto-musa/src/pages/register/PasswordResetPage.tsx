fc26a5502383159510625311639756f3a94365cf
william <castrocoralwilliam@gmail.com>
2025-10-16 15:11:04 -0500

    componente de mosaico para elegir tipo de arte

--- proyecto-musa/src/pages/register/PasswordResetPage.tsx
+++ proyecto-musa/src/pages/register/PasswordResetPage.tsx
@@ -13,11 +13,7 @@ const PasswordResetPage = () => {
   const navigate = useNavigate();
   const imageClasses = "w-full h-full object-cover transition duration-300 hover:scale-105";
 
-  const IconUser = () => (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-700">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-      </svg>
-  );
+
 
   const IconEmail = () => (
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-700">
